f19c0e62ba9753322eadfce694236638b6b20f2d
kanadetakamiya <khanh.nqa204991@sis.hust.edu.vn>
2024-12-10 19:26:52 +0700

    modified:   src/.gitignore 	modified:   src/Test.tsx 	deleted:    src/score.json

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import { Link } from "wouter";
 import OpenAI from "openai";
-import score from "./classes/score";
 
+import score from "./classes/score";
+import apiKey from "./key.json";
 import "./App.css";
-let key = window.prompt("Enter your OpenAI API key:");
-if (key == null) key = "";
-const client = new OpenAI({
-  apiKey: key,
-  dangerouslyAllowBrowser: true,
-});
 
 export function test() {
+  async function main() {
+    const client = new OpenAI({
+      apiKey: apiKey.key,
+      dangerouslyAllowBrowser: true,
+    });
+    const stream = await client.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        { role: "user", content: "Translate 'How are you' to Japanese" },
+      ],
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      console.log(chunk.choices[0].delta.content);
+    }
+  }
   return (
     <div>
       <h1>Test</h1>
@@ -21,16 +32,3 @@ export function test() {
 }
 
 export default test;
-
-async function main() {
-  const stream = await client.chat.completions.create({
-    model: "gpt-4o",
-    messages: [
-      { role: "user", content: "Translate 'How are you' to Japanese" },
-    ],
-    stream: true,
-  });
-  for await (const chunk of stream) {
-    console.log(chunk.choices[0].delta.content);
-  }
-}
